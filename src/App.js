45c7d2d925eed07844f146d6d63ba58721a8d576
mikolaj.sas <beqs171@gmail.com>
2021-04-30 14:05:26 +0200

    lab nr 8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route,Switch,Redirect} from "react-router-dom";
+import NavBar from "./components/navbar";
+import Home from "./components/home";
 import Posts from "./components/posts";
+import NotFound from "./components/notFound";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-      <main className="container">
-        <Posts/>
-      </main>
+      <div className="container-fluid">
+          <NavBar />
+          <div className="container">
+              <div className="content">
+                  <Switch>
+                      <Route path="/posts" component={Posts} />
+                      <Route path="/not-found" component={NotFound} />
+                      <Route path="/" exact component={Home} />
+                      <Redirect to="/not-found" />
+                  </Switch>
+              </div>
+          </div>
+      </div>
   );
 }
+
 
 export default App;
